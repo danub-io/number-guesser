3c706d9a0e51bcf11dfa4ade485b42823953edd3
Danubio Coelho <104590059+danub-io@users.noreply.github.com>
2022-06-04 17:40:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+//Generate random number between 0 and 9
+const generateTarget = () => {
+    secretTargetNumber = Math.floor(Math.random() * 10);
+    return secretTargetNumber;
+};
+
+//Generate random number between 0 and 9
+const generateHumanTarget = () => {
+    secretHumanNumber = Math.floor(Math.random() * 10);
+    return secretHumanNumber;
+};
+
+//Generate random number between 0 and 9
+const generateComputerTarget = () => {
+    secretComputerNumber = Math.floor(Math.random() * 10);
+    return secretComputerNumber;
+};
+
+//Compares human, computer and random numbers
+function compareGuesses(guessOne, guessTwo, guessThree) {
+    guessOne = generateTarget();
+    guessTwo = generateHumanTarget();
+    guessThree = generateComputerTarget();
+    humanDif = Math.abs(guessOne - guessTwo);
+    computerDif = Math.abs(guessOne - guessThree);
+
+    // console.log(guessOne);
+    // console.log(guessTwo);
+    // console.log(humanDif);
+    // console.log(guessThree);
+    // console.log(computerDif);
+
+    if (humanDif < computerDif) {
+        return true
+    } else {
+        return false
+    } 
+};
+
+const updateScore = () => {
+    if (compareGuesses() === true) {
+        winnerString = 'human wins';
+        return winnerString
+    } else { 
+        winnerString = 'computer wins';
+        return winnerString
+    }
+}; 
+
+const advanceRound = () => 
+    currentRoundNumber++
+
+console.log(advanceRound());
+console.log(updateScore());
